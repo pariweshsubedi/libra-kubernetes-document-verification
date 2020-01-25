c0c4bd00ab5047f4f4bf4fb858194b14a0678e9b
pariwesh <pariweshsubedi@hotmail.com>
2020-01-25 12:22:03 +0100

    initialize git

---
+++ README.md
@@ -0,0 +1,5 @@
+`transport.sh` - libra module testing requires modules to be moved to `libra/language/functional_tests/tests/testsuite/modules`. This script moves all the scripts to `language/functional_tests/tests/testsuite/modules/custom_modules` and runs the test described in the module.
+
+Requirements:
+- libra to be setup according to documentation. Current version requires it to be setup inside `~/libra` directory
+
---
+++ transport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script is responsible to transport, compile and 
+# run functional test of modules written inside modules 
+# directory.
+
+LIBRADIR="$HOME/libra"
+
+BASEDIR=$(dirname "$0")
+LIBRA_MODULES_DIR="$LIBRADIR/language/functional_tests/tests/testsuite/modules/custom_modules"
+CUSTOM_MODULES_DIR="$BASEDIR/modules/"
+
+# remove custom modules
+if [ -d "$LIBRA_MODULES_DIR" ]; then
+    rm -rf $LIBRA_MODULES_DIRr
+fi
+
+# create custom modules directory
+mkdir -p $LIBRA_MODULES_DIR
+
+#copy modules to custom_modules
+scp -rp  $CUSTOM_MODULES_DIR $LIBRA_MODULES_DIR
+
+# cd to libra directory and run the test command
+cd $LIBRADIR
+cargo test -p functional_tests modules/custom_modules
